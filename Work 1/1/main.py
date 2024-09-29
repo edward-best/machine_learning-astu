341aa03c5d41bc4ab4d5f2ca9bbba3f84f6e9989
edward-best <eduardbek88@gmail.com>
2024-09-29 23:53:05 +0400

    Добавил индексирование по данным источника

--- Work 1/1/main.py
+++ Work 1/1/main.py
@@ -1,6 +1,6 @@
 import pandas
 
-data = pandas.read_csv('Титаник.csv', encoding='utf-16-le', sep=';')
+data = pandas.read_csv('Титаник.csv', index_col='PassengerId', encoding='utf-16-le', sep=';')
 counted = data['Пол'].value_counts()
 
 print(f"На корабле плыло {counted['мужской']} мужчин и {counted['женский']} женщин")
